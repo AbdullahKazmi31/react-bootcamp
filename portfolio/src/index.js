52d2680b439db4409c2efbcc27aec9be3dd01323
abdullahkazmi31 <mkazmi.bscs16seecs@seecs.edu.pk>
2020-05-29 14:36:06 +0500

    Add all files

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -4,6 +4,7 @@ import {Router, Switch, Route} from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import App from './components/App';
 import Jokes from './components/Jokes';
+import MusicMaster from './projects/music-master';
 import Header from './components/Header';
 import './index.css';
 
@@ -12,17 +13,8 @@ ReactDOM.render(
         <Switch>
             <Route exact path='/' render={() => <Header><App /></Header>} />
             <Route path='/jokes' render={() => <Header><Jokes /></Header>} />
+            <Route path='/music-master' render={() => <Header><MusicMaster /></Header>} />
         </Switch>
-    </Router>
-    , document.getElementById('root')
-    );
-
-/* new Promise((resolve) => {
-    
-    setTimeout(() => {
-        resolve('bears, beets, battlestar');    
-    }, 2000);
-})
-.then(quote => {
-    console.log(quote);
-}) */
+    </Router>, 
+    document.getElementById('root')
+);
---
+++ portfolio/src/projects/music-master/data/socialProfiles.js
@@ -0,0 +1,23 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id:1,
+        link: 'mailto:m.abdullah.kazmi@gmail.com',
+        image: emailIcon
+    },
+    {
+        id:2,
+        link: 'https://github.com/AbdullahKazmi31',
+        image: githubIcon
+    },
+    {
+        id:3,
+        link: 'https://www.linkedin.com/in/abdullah-kazmi-ba4aa1160/',
+        image: linkedinIcon
+    }
+];
+
+export default SOCIAL_PROFILES;
